a81ade6a4b290ce2541d5f9479badcbd8d0abde7
Helder Klemp <helder.klemp@cmdsolutions.com.au>
2018-08-17 09:17:50 +1000

    feat: initial commit

---
+++ src/main/java/au/com/equifax/cicddemo/CicdDemoApplication.java
@@ -0,0 +1,12 @@
+package au.com.equifax.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/au/com/equifax/cicddemo/CicdDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package au.com.equifax.cicddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
