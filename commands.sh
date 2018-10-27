5bc575aceee757bfff3397fc5f70dc43e331ee7f
helderklemp <helderklemp@gmail.com>
2018-10-28 10:17:46 +1100

    add basic help setup

--- commands.sh
+++ commands.sh
@@ -7,21 +7,20 @@ kubectl get nodes -n kube-system
 # Running a single Pod
 kubectl apply -f k8s/deployment.yml
 kubectl get pods 
-kubectl get services
 kubectl get services cicd-demo-service -o wide
 kubectl get deployments cicd-demo-deployment -o wide
 minikube service cicd-demo-service
 
 #Running a deployment
 #window 1
-watch -n1 kubectl get pods
-kubectl scale cicd-demo-deployment --replicas=3
+watch -n1 kubectl get pods,deployment -o wide
+
 
 #window 2
 kubectl get events --watch
 
 #window 3
-while true ; do curl -s 192.168.99.100:30373/ | jq ".hostname" ; done
+while true ; do curl -s 192.168.99.100:30373// | jq ".hostname" ; done
 
 #window 4
 kubectl scale deployment cicd-demo-deployment --replicas=2
